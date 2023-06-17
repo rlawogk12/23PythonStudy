9f3383622b5f4392170bf9dccae548a7e19491a0
LeeMinHyeong <moner050>
2023-06-17 15:50:32 +0900

    :memo:update

---
+++ 05/c5-1.py
@@ -0,0 +1,3 @@
+data=list(range(1,21))
+for i in range(0,len(data)):
+    print("%d"%data[i],end=" ")
---
+++ 05/ex5-12.py
@@ -0,0 +1,5 @@
+data = [10,20,30,40,50,60,70,80]
+print(data)
+x=data.pop(2)
+print(x)
+print(data)
