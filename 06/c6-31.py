163996e7ae25b901a6ef4340560d3450e489f503
rlawogk12 <jaehag75@gmail.com>
2023-07-16 15:52:21 +0900

    :memo:update

---
+++ 06/c6-31.py
@@ -0,0 +1,7 @@
+admin_info={"id":"admin","password":"12345"}
+input_id=input("아이디를 입력하세요:")
+input_pass=input("비밀번호를 입력하세요:")
+if "id"=="admin" and "password"=="12345":
+    print("정보에 접근 권한이 있습니다!")
+else:
+    print("정보에 접근 권한이 없습니다!")
---
+++ 06/c6-41.py
@@ -0,0 +1,8 @@
+words={"꽃":"flower","나비":"butterfly","학교":"school","자동차":"car","비행기":"airplane"}
+print("<영어단어 맞추기 퀴즈>")
+for kor in words:
+    input_word=input("%s에 해당되는 영어 단어를 입력해주세요:"%kor)
+    if input_word==words[kor]:
+        print("정답입니다!")
+    else:
+        print("틀렸습니다!")
